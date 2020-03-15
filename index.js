24138311ded7d6a3c9379fe5b76151ae9b845426
YungHsiu <jeff1227alice@gmail.com>
2020-03-16 00:04:37 +0800

    [add] a primary version that can receive data from Arduino and HC-08

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// const customServiceUUID = "0000ffe0-0000-1000-8000-00805f9b34fb";
+const customServiceUUID = 0xFFE0;
+// const customServiceUUID = "0000180a-0000-1000-8000-00805f9b34fb";
+const customCharacteristicUUID = "0000ffe1-0000-1000-8000-00805f9b34fb";
+// const customCharacteristicUUID = "0xFFE1";
+
 let button_connection = document.getElementById("ble-connection");
 let button_disconnection = document.getElementById("ble-disconnection");
 let logText = document.getElementById("log");
 
+let dataBuffer = null;
 let bleDevice = null;
 let connected = false;
 
@@ -13,43 +20,100 @@ button_disconnection.addEventListener("click", (event) => {
   disconnect(event);
 });
 
-function log(v) {
-  var line = Array.prototype.slice.call(arguments).map(function(argument) {
-    return typeof argument === 'string' ? argument : JSON.stringify(argument);
-  }).join(' ');
+function log(v, mode) {
+  // var line = Array.prototype.slice.call(arguments).map(function(argument) {
+  //   return typeof argument === 'string' ? argument : JSON.stringify(argument);
+  // }).join(' ');
 
-  logText.textContent += line + '\n';
-  console.log(line);
+  if (mode === "append") {
+    logText.textContent += v + '\n';
+  } else {
+    logText.textContent = v + '\n';
+  }
+  // logText.textContent += line + '\n';
+  
+  // console.log(line);
 }
 
 function connect(event) {
-  console.log("in connect funciton!");
-  console.log(event);
   navigator.bluetooth.requestDevice({
     acceptAllDevices: true,
-    // optionalServices: ['battery_service']
+    optionalServices: [customServiceUUID]
   })
   .then(device => {
-    // console.log(device.name);
     bleDevice = device;
-    log(device.name);
+    log(`Device name: ${device.name}`);
     connected = true;
     return device.gatt.connect();
   })
+  .then(server => {
+    log('Getting Services...');
+    return server.getPrimaryService(customServiceUUID);
+    // return server.getPrimaryServices();
+  })
+  .then(service => {
+    log('Getting Characteristics...');
+    console.log(service);
+    return service.getCharacteristics(customCharacteristicUUID);
+    // let queue = Promise.resolve();
+    // services.forEach(service => {
+    //   queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+    //     log('> Service: ' + service.uuid);
+    //     characteristics.forEach(characteristic => {
+    //       log('>> Characteristic: ' + characteristic.uuid + ' ' +
+    //           getSupportedProperties(characteristic));
+    //     });
+    //   }));
+    // });
+    // return queue;
+  })
+  .then(characteristic => {
+    console.log(characteristic[0]);
+    characteristic[0].startNotifications().then(res => {
+      characteristic[0].addEventListener('characteristicvaluechanged', getData)
+    })
+  })
   .catch(err => {
-    console.log(err);
+    log(err);
   })
 }
 
 function disconnect(event) {
   if (!connected) {
+    log("No device connected");
     return;
   }
   log("Disconnecting from Bluetooth Device...");
   if (bleDevice.gatt.connected) {
     bleDevice.gatt.disconnect();
-    log("finished")
+    log("finished", "append")
   } else {
     log("> Bluetooth Device is already disconnected");
   }
+  connected = false;
+}
+
+function getData(event) {
+  console.log(event);
+  console.log(event.target.value);
+  console.log(event.target.value.buffer);
+  dataBuffer = event.target.value;
+  let bufferLength = dataBuffer.byteLength;
+  let dataString = "";
+  for (let i = 0; i < bufferLength; i++) {
+    dataString += String.fromCharCode(dataBuffer.getUint8(i));
+  }
+  log(dataString);
+}
+
+/* Utils */
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
 }
